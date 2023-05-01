677bdda6e7423e514a6a5318e94a8e9905c38d65
Shivu <122591141+Monu075@users.noreply.github.com>
2023-05-01 17:45:18 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@TheSupportChat|@MELODY_QUEEN20|@Shubhanshutya|Timesisnotwaiting|MELODY_QUEEN20|Shubhanshutya"
+GRP = "@shivu_o"
 
 GROUP = [-1001521704453, -1001410362208]
 
 VERIFIED_USERS = [936481432, 1669178360, 1941391496, 1709495698, 1920507972, 1450303652]
 
-Owners = "@Timesisnotwaiting|@MELODY_QUEEN20|Zaid"
+Owners = "@shivu_143m"
 
 PORM = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
